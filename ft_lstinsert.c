2adf5767d0f2361b165b3492a9fdaeea13a8ae09
Noureddine Abdelbadie <nabdelba@e2r8p4.1337.ma>
2019-04-22 20:26:49 +0100

    Edited

--- ft_lstinsert.c
+++ ft_lstinsert.c
@@ -1,28 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstinsert.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nabdelba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/14 18:09:54 by nabdelba          #+#    #+#             */
-/*   Updated: 2019/04/22 20:24:49 by nabdelba         ###   ########.fr       */
+/*   Created: 2019/04/22 19:54:01 by nabdelba          #+#    #+#             */
+/*   Updated: 2019/04/22 20:25:14 by nabdelba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+void	ft_lstinsert(t_list **lst, t_list *node)
 {
-	t_list	*new;
+	t_list *p;
 
-	if (!lst || !f)
-		return (NULL);
-	new = NULL;
-	while (lst)
+	if (!lst || !node)
+		return ;
+	node->next = NULL;
+	if (*lst == NULL)
+		*lst = node;
+	else
 	{
-		ft_lstinsert(&new, f(lst));
-		lst = lst->next;
+		p = *lst;
+		while (p->next)
+			p = p->next;
+		p->next = node;
 	}
-	return (new);
 }
