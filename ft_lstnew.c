1d44ff580c8916561c7903b253734ff211a201e3
Nourelddine <noureldine.98@mail.ru>
2019-06-06 16:28:03 +0000

    function fixed

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -3,37 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nabdelba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: nabdelba <nabdelba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/14 16:37:32 by nabdelba          #+#    #+#             */
-/*   Updated: 2019/04/17 20:47:09 by nabdelba         ###   ########.fr       */
+/*   Updated: 2019/06/06 16:25:45 by nabdelba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void const *content, size_t content_size)
+t_list		*ft_lstnew(void const *content, size_t content_size)
 {
-	t_list	*node;
-	char	*tmp;
+	t_list	*new;
 
-	if (!(node = (t_list *)malloc(sizeof(t_list))))
+	if (!(new = (t_list *)ft_memalloc(sizeof(t_list))))
 		return (NULL);
-	if (!content)
+	if (content == NULL)
 	{
-		node->content = NULL;
-		node->content_size = 0;
+		new->content = NULL;
+		new->content_size = 0;
 	}
 	else
 	{
-		if (!(tmp = ft_strdup((char *)content)))
-		{
-			free(node);
-			return (NULL);
-		}
-		node->content = tmp;
-		node->content_size = content_size;
+		new->content = (void *)ft_memalloc(content_size);
+		ft_memcpy(new->content, content, content_size);
+		new->content_size = content_size;
 	}
-	node->next = NULL;
-	return (node);
+	new->next = NULL;
+	return (new);
 }
